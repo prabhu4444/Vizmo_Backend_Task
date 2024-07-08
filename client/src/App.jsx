41c237219406f34ad1736d0a2a398eb39fb98369
prabhu4444 <prabhusatyam4.4@gmail.com>
2024-07-08 15:01:33 +0530

    No will use MERN only as its a small assignemnt

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Post from './component/Post'
+import Header from './component/Header'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+     <Post/>
+     <Post/>
+     <Post/>
+
+    </>
+  )
+}
+
+export default App
